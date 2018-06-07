af100364e52376e6b3712754c408156ede879bad
gollowars <>
2018-06-07 15:09:37 +0900

    add getImage to headerMixin.js

--- src/.vuepress/script/headerMixin.js
+++ src/.vuepress/script/headerMixin.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 
+const date = new Date()
+const year = date.getFullYear()
+const month = date.getMonth()
+const day = date.getDate()
+const min = date.getMinutes()
+
 export default {
+  methods: {
+    getImage(path) {
+      return `${this.$assetImage}/${path}?date=${year}${month}${day}${min}`
+    }
+  },
   computed: {
-    $title () {
+    $title() {
       const page = this.$page
       const siteTitle = this.$siteTitle
       const selfTitle = page.frontmatter.home ? null : (
@@ -11,8 +22,12 @@ export default {
       )
       return (selfTitle) ? selfTitle : siteTitle | ''
     },
-    $description () {
+    $description() {
       return this.$page.frontmatter.description || this.$localeConfig.description || this.$site.description || ''
+    },
+    $assetImage() {
+      return this.$site.themeConfig.assetImagePath
     }
   }
 }
+
